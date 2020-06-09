db52f5dec71ca94b55e9f1439123addb75537fd6
MohammadJaber <eng.mohammadjaber997@gmail.com>
2020-06-09 07:35:58 +0300

    ghj

---
+++ js/me.js
@@ -0,0 +1,52 @@
+'use strict';
+var userName = prompt('What is your name?');
+alert('Hello '+ userName)
+alert('please answer these questions with yes or no')
+var Loveprogramming = true;  //string - text
+var ans1 = prompt('Am I Love programming?').toLowerCase();
+if(ans1=='yes'.toLowerCase()){
+    alert(Loveprogramming);
+}
+else{
+    alert('false');
+}
+
+console.log('Am I Love programming?',Loveprogramming);
+var oldest_one_between_brothers = true; 
+var ans2 = prompt('Am I  + oldest one between brothers?').toLowerCase();
+if(ans2=='yes'.toLowerCase()){
+    alert(oldest_one_between_brothers);
+}
+else{
+    alert('false');
+}
+console.log('oldest_one_between_brothers ',oldest_one_between_brothers);
+var asmoker = false;  //true / false
+var ans3 = prompt('Am I a smoker?' ).toLowerCase();
+if(ans3=='yes'.toLowerCase()){
+    alert(asmoker);
+}
+else{
+    alert('true');
+}
+console.log('asmoker ',asmoker);
+var Mansaflover = true;
+var ans4 = prompt('Am I Mansaflover?' ).toLowerCase();
+if(ans4=='yes'.toLowerCase()){
+    alert(Loveprogramming);
+}
+else{
+    alert('false');
+}
+console.log('Mansaflover',Mansaflover);
+var Love_to_work_out = true;
+var ans5 = prompt('Am I Love_to_work_out?' ).toLowerCase();
+if(ans5=='yes'.toLowerCase()){
+    alert(Loveprogramming);
+}
+else{
+    alert('false');
+}
+console.log('Love_to_work_out ',Love_to_work_out);
+alert('Welcome,please keep intersted '+userName)
+
