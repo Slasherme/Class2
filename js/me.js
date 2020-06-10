749dc064ec51d730a718a8703f0805d50339018b
MohammadJaber <eng.mohammadjaber997@gmail.com>
2020-06-10 13:11:31 +0300

    asd

--- js/me.js
+++ js/me.js
@@ -5,43 +5,45 @@ alert('Hello '+ userName);
 alert('please answer these questions with yes or no');
 var Loveprogramming = true;  //string - text
 var ans1 = prompt('Am I Love programming?').toLowerCase();
-if(ans1=='yes'.toLowerCase()){
+if(ans1=='yes' .toLowerCase() || ans1=='y'.toLowerCase()){
     alert(Loveprogramming);
     myscore++;
 }
-else{
-    alert('false');
+else{if(ans1=='no' .toLowerCase() || ans1=='n'.toLowerCase()){
+    alert('false');}
 }
 
 console.log('Am I Love programming?',Loveprogramming);
 var oldest_one_between_brothers = true; 
 var ans2 = prompt('Am I  oldest one between brothers?').toLowerCase();
-if(ans2=='yes'.toLowerCase()){
+if(ans2=='yes' .toLowerCase() || ans2=='y'.toLowerCase()){
     alert(oldest_one_between_brothers);
     myscore++;}
-else{
-    alert('false');
+else{if(ans2=='no' .toLowerCase() || ans2=='n'.toLowerCase()){
+    alert('false');}
 }
 console.log('oldest_one_between_brothers ',oldest_one_between_brothers);
 var asmoker = false;  //true / false
 var ans3 = prompt('Am I a smoker?' ).toLowerCase();
-if(ans3=='yes'.toLowerCase()){
+if(ans3=='yes' .toLowerCase() || ans3=='y'.toLowerCase()){
     alert(asmoker);
     myscore++;
 }
-else{
-    alert('true');
+else{if(ans3=='no' .toLowerCase() || ans3=='n'.toLowerCase()){
+    alert('true');}
 }
 console.log('asmoker ',asmoker);
 var Mansaflover = true;
 var ans4 = prompt('Am I Mansaflover?' ).toLowerCase();
-if(ans4=='yes'.toLowerCase()){
+if(ans4=='yes' .toLowerCase() || ans4=='y'.toLowerCase()){
     alert(Loveprogramming);
     myscore++;
 }
 else{
-    alert('false');
-}
+   
+    if(ans4=='no' .toLowerCase() || ans4=='n'.toLowerCase()){
+        alert('false');
+}}
 console.log('Mansaflover',Mansaflover);
 var Love_to_work_out = true;
 var ans5 = prompt('Am I Love_to_work_out?' ).toLowerCase();
@@ -50,24 +52,36 @@ if(ans5=='yes'.toLowerCase()){
     myscore++;
 }
 else{
+    if(ans5=='no' .toLowerCase() || ans5=='n'.toLowerCase())
     alert('false');
 }
 for (var i=0 ; i <=3 ;i++)
 {var ans6 = prompt('guess the number that in my mind' ).toLowerCase();
+console.log('user entered',ans6);
+
+
 if(ans6==1){
     alert('Thats it');
     myscore++;
     break;
 }
-else{
-    alert('Opps try again');
-}}
+else if(ans6 > 7 ){
+    alert('opps too far');
+}
+else if(ans6 < 7 && ans6 !=0)
+{alert ('nearly true')}
+else {
+    alert('Not a number')
+}
+}
+
 if(i>3)
 {    alert('The answer is 1');
 }
 var Meal = ["kabab","Mansaf","Upsidedown","falafel","shawerma" .toLowerCase()] 
 for (var k=0;k <=5 ;k++)
 {var ans7 = prompt('What is my best meal that I love' .toLowerCase());
+console.log('user entered',ans7);
 if (ans7 =="kabab" .toLowerCase()|| ans7=="Mansaf" .toLowerCase() ||ans7=="Upsidedown" .toLowerCase()|| ans7=="falafel" .toLowerCase() ||ans7=="shawerma" .toLowerCase()){
     alert ('yes you are great')
     myscore++;
