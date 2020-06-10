5396cf24b9c35a119e0e1264894470b4a87aad65
MohammadJaber <eng.mohammadjaber997@gmail.com>
2020-06-10 07:23:59 +0300

    new

--- js/me.js
+++ js/me.js
@@ -1,4 +1,5 @@
 'use strict';
+var myscore=0;
 var userName = prompt('What is your name?');
 alert('Hello '+ userName);
 alert('please answer these questions with yes or no');
@@ -6,6 +7,7 @@ var Loveprogramming = true;  //string - text
 var ans1 = prompt('Am I Love programming?').toLowerCase();
 if(ans1=='yes'.toLowerCase()){
     alert(Loveprogramming);
+    myscore++;
 }
 else{
     alert('false');
@@ -16,7 +18,7 @@ var oldest_one_between_brothers = true;
 var ans2 = prompt('Am I  oldest one between brothers?').toLowerCase();
 if(ans2=='yes'.toLowerCase()){
     alert(oldest_one_between_brothers);
-}
+    myscore++;}
 else{
     alert('false');
 }
@@ -25,6 +27,7 @@ var asmoker = false;  //true / false
 var ans3 = prompt('Am I a smoker?' ).toLowerCase();
 if(ans3=='yes'.toLowerCase()){
     alert(asmoker);
+    myscore++;
 }
 else{
     alert('true');
@@ -34,6 +37,7 @@ var Mansaflover = true;
 var ans4 = prompt('Am I Mansaflover?' ).toLowerCase();
 if(ans4=='yes'.toLowerCase()){
     alert(Loveprogramming);
+    myscore++;
 }
 else{
     alert('false');
@@ -43,6 +47,7 @@ var Love_to_work_out = true;
 var ans5 = prompt('Am I Love_to_work_out?' ).toLowerCase();
 if(ans5=='yes'.toLowerCase()){
     alert(Loveprogramming);
+    myscore++;
 }
 else{
     alert('false');
@@ -51,6 +56,7 @@ for (var i=0 ; i <=3 ;i++)
 {var ans6 = prompt('guess the number that in my mind' ).toLowerCase();
 if(ans6==1){
     alert('Thats it');
+    myscore++;
     break;
 }
 else{
@@ -59,7 +65,18 @@ else{
 if(i>3)
 {    alert('The answer is 1');
 }
+var Meal = ["kabab","Mansaf","Upsidedown","falafel","shawerma" .toLowerCase()] 
+for (var k=0;k <=5 ;k++)
+{var ans7 = prompt('What is my best meal that I love' .toLowerCase());
+if (ans7 =="kabab" .toLowerCase()|| ans7=="Mansaf" .toLowerCase() ||ans7=="Upsidedown" .toLowerCase()|| ans7=="falafel" .toLowerCase() ||ans7=="shawerma" .toLowerCase()){
+    alert ('yes you are great')
+    myscore++;
+    break;
+}else{alert ('no not that')}
+}
 
+alert (Meal);
+alert (myscore + '/7');
 console.log('Love_to_work_out ',Love_to_work_out);
 alert('Welcome,please keep intersted '+userName)
 
